f2148595cfba7119d7f2a01c84a688bbc58de771
Abhijit <abhijitmondal012@gmail.com>
2026-06-25 00:56:54 +0530

    stage one

---
+++ index.js
@@ -0,0 +1,50 @@
+        
+function github() {
+    window.open('https://youtu.be/bB2o81DnKHk?si=wa0M-vAYMuhAXP34');
+}
+        
+function githubTwo() {
+        window.open('https://www.google.com');
+}
+
+function hrkv() {
+    window.open('https://www.youtube.com/watch?v=XO8SbDVJVyo')
+}
+
+function yesraj() {
+    window.open('https://www.youtube.com/watch?v=oJ6BjHU-Bec')
+}
+
+function Aieng() {
+    window.open(
+        'https://www.youtube.com/watch?v=rAtZRNs96Aw&t'
+    )
+}
+function procode() {
+    window.open(
+        'https://www.youtube.com/watch?v=zcTFtYLA_HQ&t'
+    )
+}
+function tedc() {
+    window.open(
+        'https://www.youtube.com/watch?v=yhGzXULZkEw'
+    )
+}
+
+function elon() {
+    window.open(
+        'https://www.youtube.com/watch?v=2H4z1g-eHyU&pp=ygUTZWxvbiBtdXNrIGludGVydmlldw%3D%3D'
+    )
+}
+
+function prointer() {
+    window.open(
+        'https://www.youtube.com/watch?v=7ikbE00CAro'
+    )
+}
+
+function zerosd() {
+    window.open(
+        'https://www.youtube.com/watch?v=WR1ydijTx5E'
+    )
+}
